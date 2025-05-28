17e343c83d6a361f2b08ca798e53b3138cc0cc71
Luisrc07 <luisrodriguezc0702@gmail.com>
2025-05-28 10:51:11 -0400

    actualizacion

--- advpro-app/resources/views/components/app-layout.blade.php
+++ advpro-app/resources/views/components/app-layout.blade.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- TAILWIND -->
     <script src="https://cdn.tailwindcss.com"></script>
-      <!-- BOOTSTRAP -->
+    <!-- BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
     
     <title>ADVPRO</title>
-   </head>
+    </head>
 <body class="bg-gray-100">
     <div class="bg-white flex p-6 shadow-sm">
         <h6 class="flex-grow text-2xl font-bold">AudioVisual Pro</h6>
