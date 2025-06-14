3c98a3fb7954b1ee32b466aab4f2ea20db796293
Luisrc07 <luisrodriguezc0702@gmail.com>
2025-06-14 19:20:58 -0400

    Solucion rutas login y registro

--- advpro-app/resources/views/components/app-layout.blade.php
+++ advpro-app/resources/views/components/app-layout.blade.php
@@ -203,18 +203,15 @@
                   class="p-2 mt-2 space-y-2 overflow-hidden text-sm font-medium text-gray-500 rounded-md shadow-inner bg-gray-50 dark:text-gray-400 dark:bg-gray-900"
                   aria-label="submenu"
                 >
-                  <li
-                    class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                  >
-                    <a class="w-full" href="pages/login.html">Login</a>
+                  <li class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
+                    <a class="w-full" href="{{ route('login') }}">Login</a>
                   </li>
-                  <li
-                    class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                  >
-                    <a class="w-full" href="pages/create-account.html">
-                      Create account
-                    </a>
+
+                 <li class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
+                     <a class="w-full" href="{{ route('register') }}">Registro</a>
                   </li>
+
+
                   <li
                     class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
                   >
