5bab1351a2d300b36a111bad870698c9c517a212
DogeVzla <baironddiaz@gmail.com>
2025-06-16 18:26:40 -0400

    Correcciones (Error en Proyecto.php)

--- advpro-app/app/Models/EstadoContrato.php
+++ advpro-app/app/Models/EstadoContrato.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class forma_pago extends Model
+Class EstadoContrato extends Model
 {
     //
 }
--- advpro-app/app/Models/staff.php
+++ advpro-app/app/Models/staff.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class staff extends Model
+class Staff extends Model
 {
     
     protected $table = 'staff';
